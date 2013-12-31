9adf2abff232788bc143f2a47ff096769b9e090f
Simon Staszkiewicz <simonstaszkiewicz@gmail.com>
2013-12-31 14:28:59 -0800

    Fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var $vid = $("#video")
 	$("#frame").load(function(){
 		setTimeout(function(){
-			$vid.fadeIn("slow").animate({
+			$vid.animate({
 				marginLeft: "-310px",
 				opacity: 1
 			}, 500);
